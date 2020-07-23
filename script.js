03717b810076f924b399dd7afce07d7b9811f798
mrasahi <makotorasahi@gmail.com>
2020-07-23 15:45:50 -0700

    initial setup

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+let lowerCase = `abcdefghijklmnopqrstuvwxyz` // lowerCase[ 0 - 25 ]
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' // upperCase[ 0 - 25 ]
+let numeric = '1234567890' // numeric[ 0 - 9]
+let special = "#$%&'()*+,-./:;<=>?@[]^!_`{|}~" // [ 0 - 29] double quote and backslash are excluded
+let passwordLength // min 8 max 128
+let password
+let possibleCharacters
+
+console.log(special[29])
