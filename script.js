8628435d0ecfb6f9652b5b5824ff19d2f561681e
Mak <makotorasahi@gmail.com>
2020-07-23 16:40:59 -0700

    added prompt length and check

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
 let lowerCase = `abcdefghijklmnopqrstuvwxyz` // lowerCase[ 0 - 25 ]
 let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' // upperCase[ 0 - 25 ]
 let numeric = '1234567890' // numeric[ 0 - 9]
 let special = "#$%&'()*+,-./:;<=>?@[]^!_`{|}~" // [ 0 - 29] double quote and backslash are excluded
-let passwordLength // min 8 max 128
-let password
-let possibleCharacters
+let passwordLength  // min 8 max 128
+let possibleCharacters = ''
+let PasswordText =''
 
-console.log(special[29])
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+
+
+
+
+function promptLength() {
+  passwordLength = parseInt(prompt('How long do you want your password? Min 8 Max 128'))
+  if (passwordLength) {
+    alert('Please enter a number')
+    return
+  }
+  if (passwordLength > 8 || passwordLength < 128) {
+    alert('Please enter a number between 8 and 128')
+    return
+  }
+}
+
+
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+generateBtn.addEventListener("click", () => {
+  promptLength()
+  // function 4
+  // writePassword;
+})
+
